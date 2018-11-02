34d425bfafce71404fd96ae3b833aed6ac628f70
Shogun <natan2337@mail.ru>
2018-11-02 18:25:02 +0400

    Created comment model

--- honor/urls.py
+++ honor/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 
+
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+
 ]
